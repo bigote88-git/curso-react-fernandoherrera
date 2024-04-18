fc802816425f5189b17a93b2e85f4c87e5763db3
djcortez <djcortez@inss.gob.ni>
2024-04-18 16:40:24 -0600

    Incluyendo testing

---
+++ 01-counter-app/src/tests/demo.test.js
@@ -0,0 +1,11 @@
+describe('Pruebas random', () => {
+
+    test('this is a sample test', () => {
+        // 1. Inicializacion
+        const value1 = "25";
+        // 2. Estimulo
+        const value2 = "25"
+            // 3. Observar el comportamiento
+        expect(value1).toBe(value2);
+    })
+})
